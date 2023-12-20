8869b30264391f8a2d274f736131d544da0bca15
Presti <dxssucuk@hotmail.com>
2023-12-20 14:37:28 +0100

    Initial commit

---
+++ src/main/java/de/presti/shizomc/ShizoMC.java
@@ -0,0 +1,17 @@
+package de.presti.shizomc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShizoMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
