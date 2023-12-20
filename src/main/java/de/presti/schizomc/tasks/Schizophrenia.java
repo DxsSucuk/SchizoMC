50ab6803d473f532c6391055154f71af9d7c9f39
Presti <dxssucuk@hotmail.com>
2023-12-20 17:04:23 +0100

    Adding the deletion of any entity that is out of side for players below the sanity of 55%

--- src/main/java/de/presti/schizomc/tasks/Schizophrenia.java
+++ src/main/java/de/presti/schizomc/tasks/Schizophrenia.java
@@ -3,14 +3,15 @@ package de.presti.schizomc.tasks;
 import de.presti.schizomc.utils.ArrayUtils;
 import de.presti.schizomc.utils.SchizoUtil;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Schizophrenia extends BukkitRunnable {
-
 
     @Override
     public void run() {
@@ -35,5 +36,46 @@ public class Schizophrenia extends BukkitRunnable {
                 block.getBlock().setType(Material.AIR);
             }
         });
+
+        List<Entity> entities = players.stream().filter(playerFloatEntry -> playerFloatEntry.getValue() <= 0.55).map(Map.Entry::getKey).toList().stream().flatMap(p -> p.getNearbyEntities(10, 10, 10).stream()).toList();
+
+        List<Entity> toDeleteEntity = new ArrayList<>();
+
+        entities.forEach(entity -> {
+            if (entity instanceof Player) {
+                return;
+            }
+
+            if (toDeleteEntity.contains(entity)) {
+                return;
+            }
+
+            if (entity.isDead()) {
+                return;
+            }
+
+            if (toDeleteEntity.stream().anyMatch(c -> c.getUniqueId() == entity.getUniqueId())) {
+                return;
+            }
+
+            boolean shouldDelete = true;
+
+            for (Player player : players.stream().filter(playerFloatEntry -> playerFloatEntry.getValue() >= 0.85).map(Map.Entry::getKey).toList()) {
+                if (entity.getWorld().equals(player.getWorld())) {
+                    if (entity.getLocation().distance(player.getLocation()) <= 25) {
+                        if (!SchizoUtil.locationNotVisible(player.getEyeLocation().getDirection(), player.getEyeLocation().toVector(), entity.getLocation().toVector())) {
+                            shouldDelete = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (shouldDelete) {
+                toDeleteEntity.add(entity);
+            }
+        });
+
+        toDeleteEntity.forEach(Entity::remove);
     }
 }
